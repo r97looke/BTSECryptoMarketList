7e4616b6cafb9bf1edd7ab20c6375fc07d4c7c58
Shun Lung Chen <r97looke@gmail.com>
2023-08-16 14:19:52 +0800

    Create empty CryptoMarket App

---
+++ CryptoMarketPriceApp/CryptoMarketPriceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoMarketPriceApp
+//
+//  Created by Shun Lung Chen on 2023/8/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
