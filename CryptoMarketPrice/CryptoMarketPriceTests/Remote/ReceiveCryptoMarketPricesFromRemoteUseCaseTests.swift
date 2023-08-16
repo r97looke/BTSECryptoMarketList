745219cd732d9bc091f6db2a56d79a84d6fb093d
Shun Lung Chen <r97looke@gmail.com>
2023-08-16 09:40:11 +0800

    RemoteCryptoMarketPricesReceiver delegate error on client connect error

--- CryptoMarketPrice/CryptoMarketPriceTests/Remote/ReceiveCryptoMarketPricesFromRemoteUseCaseTests.swift
+++ CryptoMarketPrice/CryptoMarketPriceTests/Remote/ReceiveCryptoMarketPricesFromRemoteUseCaseTests.swift
@@ -7,27 +7,68 @@
 
 import XCTest
 
+protocol WebsocketClientDelegate: AnyObject {
+    func websocketConnectError(error: Error)
+}
+
 protocol WebsocketClient {
-    
+    var delegate: WebsocketClientDelegate? { set get }
 }
 
 final class WebsocketClientSpy: WebsocketClient {
     
-    var connectCallCount = 0
+    weak var delegate: WebsocketClientDelegate?
+    
+    var requestConnectURLs = [URL]()
+    
+    func connect(url: URL) {
+        requestConnectURLs.append(url)
+    }
+    
+    func completeConnect(with error: Error) {
+        delegate?.websocketConnectError(error: error)
+    }
 }
 
-protocol CryptoMarketPricesReceiver {
-    
+protocol CryptoMarketPricesReceiverDelegate: AnyObject {
+    func receiverDidFinishConnect(with error: Error)
+}
+
+protocol CryptoMarketPricesReceiver: WebsocketClientDelegate {
+    var delegate: CryptoMarketPricesReceiverDelegate? { get set }
 }
 
 final class RemoteCryptoMarketPricesReceiver: CryptoMarketPricesReceiver {
     
     let url: URL
-    let client: WebsocketClient
+    let client: WebsocketClientSpy
+    weak var delegate: CryptoMarketPricesReceiverDelegate?
     
-    init(url: URL, client: WebsocketClient) {
+    init(url: URL, client: WebsocketClientSpy) {
         self.url = url
         self.client = client
+        client.delegate = self
+    }
+    
+    func startReceive() {
+        client.connect(url: url)
+    }
+    
+    // MARK: WebsocketClientDelegate
+    func websocketConnectError(error: Error) {
+        delegate?.receiverDidFinishConnect(with: error)
+    }
+}
+
+final class CryptoMarketPricesReceiverDelegateSpy: CryptoMarketPricesReceiverDelegate {
+    enum Message: Equatable {
+        case connectError
+    }
+    
+    var message = [Message]()
+    
+    func receiverDidFinishConnect(with error: Error) {
+        message.append(.connectError)
     }
 }
 
@@ -37,7 +78,30 @@ final class ReceiveCryptoMarketPricesFromRemoteUseCaseTests: XCTestCase {
         let client = WebsocketClientSpy()
         let _ = RemoteCryptoMarketPricesReceiver(url: anyWebsocketURL(), client: client)
         
-        XCTAssertEqual(client.connectCallCount, 0)
+        XCTAssertEqual(client.requestConnectURLs, [])
+    }
+    
+    func test_startReceive_requestConnectToTheURL() {
+        let url = anyWebsocketURL()
+        let client = WebsocketClientSpy()
+        let sut = RemoteCryptoMarketPricesReceiver(url: url, client: client)
+        
+        sut.startReceive()
+        
+        XCTAssertEqual(client.requestConnectURLs, [url])
+    }
+    
+    func test_startReceive_deliversErrorOnConnectError() {
+        let url = anyWebsocketURL()
+        let client = WebsocketClientSpy()
+        let sut = RemoteCryptoMarketPricesReceiver(url: url, client: client)
+        let delegateSpy = CryptoMarketPricesReceiverDelegateSpy()
+        sut.delegate = delegateSpy
+        
+        sut.startReceive()
+        
+        client.completeConnect(with: anyNSError())
+        XCTAssertEqual(delegateSpy.message, [.connectError])
     }
     
     // MARK: Helpers
