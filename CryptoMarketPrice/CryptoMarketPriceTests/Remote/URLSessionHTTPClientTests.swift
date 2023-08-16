90ba099291408a57c6e9b46b1d41430a522cdc0e
Shun Lung Chen <r97looke@gmail.com>
2023-08-16 08:33:26 +0800

    Add empty URLSessionHTTPClientTests

---
+++ CryptoMarketPrice/CryptoMarketPriceTests/Remote/URLSessionHTTPClientTests.swift
@@ -0,0 +1,12 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  CryptoMarketPriceTests
+//
+//  Created by Shun Lung Chen on 2023/8/16.
+//
+
+import XCTest
+
+final class URLSessionHTTPClientTests: XCTestCase {
+
+}
