bdd395786a8bc2204d7d316d53af92b830401e77
Shun Lung Chen <r97looke@gmail.com>
2023-08-18 10:48:20 +0800

    Fix cocoa pods warning

--- CryptoMarketPriceApp/Pods/Target Support Files/Pods-CryptoMarketPriceApp-CryptoMarketPriceAppUITests/Pods-CryptoMarketPriceApp-CryptoMarketPriceAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CryptoMarketPriceApp_CryptoMarketPriceAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CryptoMarketPriceApp_CryptoMarketPriceAppUITests
-@end
